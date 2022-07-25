2453fb1eed90a8abd7f6c7222344f2d03e0a90d0
Leonardo Pessoa Freitas e Silva <103889950+leonardopessoa-eng@users.noreply.github.com>
2022-07-25 10:27:22 -0300

    Update main_file.py

--- main_file.py
+++ main_file.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Helthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text(🍜 'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔 'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞 'Avocado Toast')
+streamlit.text('🍜 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
