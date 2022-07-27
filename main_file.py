ca487db7abdf34e139fa7904ffadc40e769f302c
Leonardo Pessoa Freitas e Silva <103889950+leonardopessoa-eng@users.noreply.github.com>
2022-07-27 08:27:29 -0300

    Update main_file.py
    Request and print response status

--- main_file.py
+++ main_file.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas as pd
+import requests
 
 streamlit.title('My Parents New Helthy Diner')
 streamlit.header('Breakfast Menu')
@@ -9,7 +11,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -19,3 +20,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
