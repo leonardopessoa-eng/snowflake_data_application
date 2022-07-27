2007d58b33466663e3b2fde4a42595abd579c837
Leonardo Pessoa Freitas e Silva <103889950+leonardopessoa-eng@users.noreply.github.com>
2022-07-27 08:38:49 -0300

    Update main_file.py
    " + "kiwi"

--- main_file.py
+++ main_file.py
@@ -21,9 +21,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json())
 
+streamlit.header('Fruitvice Fruit Advice')
 # Creating a DataFrame from the JSON file.
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 # Creating a DataFrame in the Streamlit.
